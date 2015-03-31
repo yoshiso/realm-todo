d9f04ae6b38e245ddf8b7f8196d1d287d7acf6d1
yss44 <nya060@gmail.com>
2015-03-31 12:27:15 +0900

    quick specable

--- realm-todoTests/realm_todoTests.swift
+++ realm-todoTests/realm_todoTests.swift
@@ -6,31 +6,25 @@
 //  Copyright (c) 2015年 ShoYoshida. All rights reserved.
 //
 
-import UIKit
-import XCTest
+import Quick
+import Nimble
 
-class realm_todoTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+class ToDoItemSpec : QuickSpec {
+    override func spec() {
+        var todoItem: ToDoItem?
+        
+        beforeEach { () -> () in
+            todoItem = ToDoItem()
         }
+        
+        describe("attributes", { () -> Void in
+            
+            it("has attr name") {
+                expect(todoItem!.name).to(equal(""))
+            }
+        })
+
+
+        
     }
-    
 }
