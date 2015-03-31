6827e995ae30295165d51542fe5a89128aecd8a1
yss44 <nya060@gmail.com>
2015-04-01 02:22:36 +0900

    update model desc

--- realm-todo/ToDoItem.swift
+++ realm-todo/ToDoItem.swift
@@ -10,7 +10,13 @@ import UIKit
 import Realm
 
 class ToDoItem: RLMObject {
-    
+    // model層
+    // stateとして is_visible_on_clientを持つ
+    // 更新シークエンス
+    // 0. 原則としてすべてのデータをクライアントに配信する(データは削除されない)
+    // 1. 最新のデータをフェッチしてくる
+    // 2. 全件保存、クライアントはis_visible_on_clientのデータのみを表示する
+    // ex) 後々の改修でデータボリュームが大きくなってきた場合に改修する
     dynamic var id = 0
     dynamic var name = ""
     dynamic var finished = false
